3dc5c4db7b33ff42b83acdc0f285a2c194df3568
r@user <108922357+realmeuser@users.noreply.github.com>
2025-03-22 22:40:36 +0700

    update

--- install-theme.sh
+++ install-theme.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Termux Auto-ZSH Setup
+# Termux Auto-ZSH Setup with Gray Theme
 # Repo: https://github.com/r0users/Termify
 
 # Suppress welcome message
@@ -7,11 +7,27 @@ touch ~/.hushlogin
 
 # Core setup
 pkg update -y && pkg upgrade -y
-pkg install -y zsh
+pkg install -y zsh git
 chsh -s zsh
 
 # Install Oh My ZSH (silent mode)
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+
+# Install autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+
+# Configure theme and colors
+cat > ~/.zshrc <<EOL
+# Termify Configuration
+ZSH_THEME="robbyrussell"
+plugins=(git zsh-autosuggestions)
+source \$ZSH/oh-my-zsh.sh
+EOL
+
+# Set gray background (#333333)
+mkdir -p ~/.termux
+echo -e "background=#333333\nforeground=#FFFFFF" > ~/.termux/colors.properties
+termux-reload-settings
 
 # Clean exit
 exit
